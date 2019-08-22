bdc6a248f16a26c0a005984955619e7e62e230de
mkbishawi <bishawi1@gmail.com>
2019-08-22 20:11:53 +0300

    Chapter 10

--- LearnNetCore/Startup.cs
+++ LearnNetCore/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
 namespace LearnNetCore
 {
     public class Startup
@@ -30,19 +32,26 @@ namespace LearnNetCore
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Use(async (context,NextDel) =>
+            {
+                await context.Response.WriteAsync("Hello From 1st MiddleWare! ");
+                await NextDel();
+            });
+
             app.Run(async (context) =>
             {
-                string strMessage = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+                //string strMessage = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
 
-                if (env.IsDevelopment())
-                {
-                    strMessage = _config["MyKey"];
-                }
-                else if (env.IsStaging()) 
-                {
-                    strMessage = "Staging";
-                }
-                await context.Response.WriteAsync("Hello World! " + System.Environment.NewLine + strMessage);
+                //if (env.IsDevelopment())
+                //{
+                //    strMessage = _config["MyKey"];
+                //}
+                //else if (env.IsStaging()) 
+                //{
+                //    strMessage = "Staging";
+                //}
+                //await context.Response.WriteAsync("Hello World! " + System.Environment.NewLine + strMessage);
+                await context.Response.WriteAsync("Hello From 2nd MiddleWare! ");
             });
         }
     }
